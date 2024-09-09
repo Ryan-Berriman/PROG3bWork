051489811ce34298af86b1aaa3ed32c9f1e44815
Ryan-Berriman <ST10044691@vcconnect.edu.za>
2024-09-09 10:23:27 +0200

    upload to github

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitActionsTester
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing");
+
+            Console.ReadLine();
+
+        }
+    }
+}
